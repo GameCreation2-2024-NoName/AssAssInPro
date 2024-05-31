10b3108ee8d297ed9699d50eac35df83381d59c7
Pditine <hanfe666@outlook.com>
2024-05-31 21:52:29 +0800

    进入单人教程，教程场景临时更改

---
+++ Assets/Pditine/Scripts/Menu/OneOrTwoPlayer/OneOrTwoPlayerMenu.cs
@@ -0,0 +1,17 @@
+﻿using Pditine.Data;
+using Pditine.Data.GameModule;
+using PurpleFlowerCore;
+using UnityEngine;
+
+namespace Pditine.Menu.OneOrTwoPlayer
+{
+    public class OneOrTwoPlayerMenu : MonoBehaviour
+    {
+        [SerializeField] private GameModelBase theGameModel;
+        public void StartSinglePlayerGame()
+        {
+            DataManager.Instance.PassingData.currentGameModel = theGameModel;
+            SceneSystem.LoadScene(DataManager.Instance.PassingData.currentGameModel.GetARandomScene());
+        }
+    }
+}
---
+++ Assets/Pditine/Scripts/Test/SimulatePlayerJoin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+
+namespace Pditine.Test
+{
+    public class SimulatePlayerJoin : MonoBehaviour
+    {
+        void Start()
+        {
+            // 模拟玩家加入
+            SimulatePlayerJoinInput();
+        }
+
+        private void SimulatePlayerJoinInput()
+        {
+            // 创建一个虚拟设备
+            var virtualDevice = InputSystem.AddDevice<Keyboard>();
+
+            // 创建一个InputUser并将虚拟设备分配给它
+            var user = InputUser.PerformPairingWithDevice(virtualDevice);
+
+            // 使用虚拟设备触发玩家加入
+            PlayerInputManager.instance.JoinPlayer(playerIndex: 1, pairWithDevice: virtualDevice);
+        }
+    }
+}
