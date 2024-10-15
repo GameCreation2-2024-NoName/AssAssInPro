cf26736d982fe0f2701e30fdf8c82df45c5cc2e6
紫地丁 <jianhaoli@lilith.com>
2024-10-15 21:11:56 +0800

    [chore]综合数值控制器支持各种类型，以及类型继承

--- Assets/PurpleFlowerCore/Editor/Tool/Data/DataController.cs
+++ Assets/PurpleFlowerCore/Editor/Tool/Data/DataController.cs
@@ -1,11 +1,13 @@
 #if UNITY_EDITOR
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using PurpleFlowerCore;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Pditine.Tool
 {
@@ -142,51 +144,287 @@ namespace Pditine.Tool
 
         private void ShowObject()
         {
-            if (_currentData != null)
-            {
-                Type targetType = _currentData.GetType();
-                
-                FieldInfo[] fields =
-                    targetType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
-                EditorGUI.BeginChangeCheck();
-                
-                foreach (FieldInfo field in fields)
+            ShowObject(_currentData);
+        }
+
+        private void ShowObject(ScriptableObject data, Type type = null)
+        {
+            var currentData = data;
+            if (currentData == null) return;
+            // if (type != null && type != typeof(ScriptableObject))
+            // {
+            //     ShowObject(data, type.BaseType);
+            // }else if(currentData.GetType().BaseType != typeof(ScriptableObject))
+            // {
+            //     ShowObject(data, currentData.GetType().BaseType);
+            // }
+            if (type == null)
+            {
+                if(currentData.GetType().BaseType != typeof(ScriptableObject))
+                    ShowObject(data, currentData.GetType().BaseType);
+            }else if(type.BaseType != typeof(ScriptableObject))
+            {
+                ShowObject(data, type.BaseType);
+            }
+            
+            Type targetType = type ?? currentData.GetType();
+            
+            FieldInfo[] fields =
+                targetType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            EditorGUI.BeginChangeCheck();
+            
+            foreach (FieldInfo field in fields)
+            {
+                if (field.IsPublic || Attribute.IsDefined(field, typeof(SerializeField)))
                 {
-                    if (field.IsPublic || Attribute.IsDefined(field, typeof(SerializeField)))
-                    {
-                        Type fieldType = field.FieldType;
-                        if (fieldType == typeof(int))
-                        {
-                            int value = (int)field.GetValue(_currentData);
-                            value = EditorGUILayout.IntField(field.Name, value);
-                            field.SetValue(_currentData, value);
-                        }
-                        else if (fieldType == typeof(float))
-                        {
-                            float value = (float)field.GetValue(_currentData);
-                            value = EditorGUILayout.FloatField(field.Name, value);
-                            field.SetValue(_currentData, value);
-                        }
-                        else if (fieldType == typeof(string))
-                        {
-                            string value = (string)field.GetValue(_currentData);
-                            value = EditorGUILayout.TextField(field.Name, value);
-                            field.SetValue(_currentData, value);
-                        }
-                        else if (fieldType == typeof(bool))
-                        {
-                            bool value = (bool)field.GetValue(_currentData);
-                            value = EditorGUILayout.Toggle(field.Name, value);
-                            field.SetValue(_currentData, value);
-                        }
-                    }
-                }
-                if (EditorGUI.EndChangeCheck())
-                {
-                    EditorUtility.SetDirty(_currentData);
+                    ShowField(field, currentData);
                 }
             }
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(currentData);
+            }
+            
+        }
+
+        private void ShowField(FieldInfo field, object target)
+        {
+            Type fieldType = field.FieldType;
+            object value = field.GetValue(target);
+
+            if (fieldType == typeof(int))
+            {
+                field.SetValue(target, EditorGUILayout.IntField(field.Name, (int)value));
+            }
+            else if (fieldType == typeof(float))
+            {
+                field.SetValue(target, EditorGUILayout.FloatField(field.Name, (float)value));
+            }
+            else if (fieldType == typeof(string))
+            {
+                field.SetValue(target, EditorGUILayout.TextField(field.Name, (string)value));
+            }
+            else if (fieldType == typeof(bool))
+            {
+                field.SetValue(target, EditorGUILayout.Toggle(field.Name, (bool)value));
+            }
+            else if (fieldType.IsEnum)
+            {
+                field.SetValue(target, EditorGUILayout.EnumPopup(field.Name, (Enum)value));
+            }
+            else if (typeof(UnityEventBase).IsAssignableFrom(fieldType))
+            {
+                ShowUnityEvent(field, target);
+            }
+            else if (fieldType.IsArray)
+            {
+                ShowArray(field, target);
+            }
+            else if (typeof(IList).IsAssignableFrom(fieldType))
+            {
+                ShowList(field, target);
+            }
+            else if (fieldType.IsClass || fieldType.IsValueType)
+            {
+                ShowClassOrStruct(field, target);
+            }
+            else
+            {
+                EditorGUILayout.LabelField(field.Name, $"Unsupported type: {fieldType}");
+            }
+        }
+
+        private void ShowUnityEvent(FieldInfo field, object target)
+        {
+            SerializedObject serializedObject = new SerializedObject((UnityEngine.Object)target);
+            SerializedProperty property = serializedObject.FindProperty(field.Name);
+            if (property != null)
+            {
+                EditorGUILayout.PropertyField(property, new GUIContent(field.Name), true);
+                serializedObject.ApplyModifiedProperties();
+            }
+            else
+            {
+                EditorGUILayout.LabelField(field.Name, "Could not find SerializedProperty for UnityEvent");
+            }
+        }
+
+        private void ShowArray(FieldInfo field, object target)
+        {
+            Array array = (Array)field.GetValue(target);
+            Type elementType = field.FieldType.GetElementType();
+            int newSize = EditorGUILayout.IntField($"{field.Name} Size", array.Length);
+            if (newSize != array.Length)
+            {
+                Array newArray = Array.CreateInstance(elementType, newSize);
+                Array.Copy(array, newArray, Math.Min(array.Length, newArray.Length));
+                array = newArray;
+                field.SetValue(target, array);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                object element = array.GetValue(i);
+                EditorGUILayout.LabelField($"   {field.Name}[{i}]");
+                ShowField(elementType, element, newValue => array.SetValue(newValue, i));
+            }
+        }
+
+        private void ShowList(FieldInfo field, object target)
+        {
+            try
+            {
+                IList list = (IList)field.GetValue(target);
+                Type elementType = field.FieldType.GetGenericArguments()[0];
+                int newSize = EditorGUILayout.IntField($"{field.Name} Size", list.Count);
+                if (newSize != list.Count)
+                {
+                    while (newSize > list.Count)
+                    {
+                        if(elementType == typeof(string))
+                            list.Add("");
+                        else
+                            list.Add(Activator.CreateInstance(elementType));
+                    }
+                    while (newSize < list.Count)
+                    {
+                        list.RemoveAt(list.Count - 1);
+                    }
+                }
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    object element = list[i];
+                    EditorGUILayout.LabelField($"   {field.Name}[{i}]");
+                    ShowField(elementType, element, newValue => list[i] = newValue);
+                }
+            }
+            catch (Exception e)
+            {
+                EditorGUILayout.LabelField(field.Name, $"Error: {e.Message}");
+            }
+        }
+
+        private void ShowClassOrStruct(FieldInfo field, object target)
+        {
+            object value = field.GetValue(target);
+            if (value == null)
+            {
+                value = Activator.CreateInstance(field.FieldType);
+                field.SetValue(target, value);
+            }
+
+            EditorGUI.indentLevel++;
+            EditorGUILayout.LabelField(field.Name, EditorStyles.boldLabel);
+            FieldInfo[] subFields = field.FieldType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var subField in subFields)
+            {
+                ShowField(subField, value);
+            }
+            EditorGUI.indentLevel--;
+        }
+
+        private void ShowField(Type fieldType, object value, Action<object> setValue)
+        {
+            if (fieldType == typeof(int))
+            {
+                setValue(EditorGUILayout.IntField((int)value));
+            }
+            else if (fieldType == typeof(float))
+            {
+                setValue(EditorGUILayout.FloatField((float)value));
+            }
+            else if (fieldType == typeof(string))
+            {
+                setValue(EditorGUILayout.TextField((string)value));
+            }
+            else if (fieldType == typeof(bool))
+            {
+                setValue(EditorGUILayout.Toggle((bool)value));
+            }
+            else if (fieldType.IsEnum)
+            {
+                setValue(EditorGUILayout.EnumPopup((Enum)value));
+            }
+            else if (fieldType.IsArray)
+            {
+                ShowArray(fieldType, (Array)value, setValue);
+            }
+            else if (typeof(IList).IsAssignableFrom(fieldType))
+            {
+                ShowList(fieldType, (IList)value, setValue);
+            }
+            else if (fieldType.IsClass || fieldType.IsValueType)
+            {
+                ShowClassOrStruct(fieldType, value, setValue);
+            }
+            else
+            {
+                EditorGUILayout.LabelField($"Unsupported type: {fieldType}");
+            }
+        }
+
+        private void ShowArray(Type elementType, Array array, Action<Array> setValue)
+        {
+            int newSize = EditorGUILayout.IntField("Size", array.Length);
+            if (newSize != array.Length)
+            {
+                Array newArray = Array.CreateInstance(elementType, newSize);
+                Array.Copy(array, newArray, Math.Min(array.Length, newArray.Length));
+                array = newArray;
+                setValue(array);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                object element = array.GetValue(i);
+                ShowField(elementType, element, newValue => array.SetValue(newValue, i));
+            }
+        }
+
+        private void ShowList(Type elementType, IList list, Action<IList> setValue)
+        {
+            int newSize = EditorGUILayout.IntField("Size", list.Count);
+            if (newSize != list.Count)
+            {
+                while (newSize > list.Count)
+                {
+                    if(elementType == typeof(string))
+                        list.Add("");
+                    else
+                        list.Add(Activator.CreateInstance(elementType));
+                }
+                while (newSize < list.Count)
+                {
+                    list.RemoveAt(list.Count - 1);
+                }
+                setValue(list);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                object element = list[i];
+                ShowField(elementType, element, newValue => list[i] = newValue);
+            }
+        }
+
+        private void ShowClassOrStruct(Type fieldType, object value, Action<object> setValue)
+        {
+            if (value == null)
+            {
+                value = Activator.CreateInstance(fieldType);
+                setValue(value);
+            }
+
+            EditorGUI.indentLevel++;
+            FieldInfo[] subFields = fieldType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var subField in subFields)
+            {
+                ShowField(subField, value);
+            }
+            EditorGUI.indentLevel--;
         }
     }
 }
