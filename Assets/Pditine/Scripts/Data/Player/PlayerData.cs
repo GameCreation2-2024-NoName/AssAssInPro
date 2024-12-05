2c0f4da1013a001cad064a31beb5ce7fa5c9f1b6
紫地丁 <hanfe666@outlook.com>
2024-12-05 21:24:04 +0800

    [feat] 剥离玩家本身的可配置数据

---
+++ Assets/Pditine/Scripts/Data/Player/PlayerData.cs
@@ -0,0 +1,13 @@
+// -------------------------------------------------
+// Copyright@ The Most Devout Followers of McDonald
+// Author : lijianhao
+// Date: 2024_12_05
+// -------------------------------------------------
+
+namespace Pditine.Data.Player
+{
+    public class PlayerData
+    {
+        
+    }
+}
